251f4a677b91a45404f54495848bdc38d588bb75
ishaan-process <ishaan2014@gmail.com>
2020-01-05 15:59:20 +0530

    obstacle added

--- Rockets.js
+++ Rockets.js
@@ -1,8 +1,15 @@
 var population;
-var lifespan = 250;
+var lifespan = 400;
 var count = 0;
 var lifeP;
 var target;
+var maxForce = 0.1;
+
+
+var rx = 100;
+var ry = 150;
+var rw = 200;
+var rh = 10;
 
 function setup(){
     createCanvas(400,300);
@@ -24,7 +31,10 @@ function draw(){
         // population = new Population();
     count = 0;
     }
-    ellipse(target.x, target.y, 20,20);
+    fill(255);
+    rect(100,150,200,10);
+
+    ellipse(target.x, target.y, 16,16);
 
 }
 
@@ -47,8 +57,9 @@ function Population(dna){
                 maxfit = this.rockets[i].fitness;
             }
     }
-    createP(maxfit);
-    
+  //  createP(maxfit);
+    console.log(this.rockets);
+
     for(var i=0;i<this.popsize;i++){
         this.rockets[i].fitness /= maxfit;
     }
@@ -70,6 +81,7 @@ function Population(dna){
     var parentA = random(this.matingpool).dna;
     var parentB = random(this.matingpool).dna;
     var child = parentA.crossover(parentB)
+    child.mutation();
     newRockets[i] = new Rocket(child)
     }
     this.rockets = newRockets;
@@ -87,12 +99,16 @@ function Population(dna){
     }
 }
 
-function DNA(){
-    this.genes = [];
+function DNA(genes){
+    if(genes)
+         this.genes = genes;
+    else{
+         this.genes = [];
     for(var i =0;i<lifespan; i++){
         this.genes[i] = p5.Vector.random2D();
         this.genes[i].setMag(0.1);
-    }    
+    }  
+}  
 
     this.crossover = function(partner){
         var newgenes = [];
@@ -105,6 +121,13 @@ function DNA(){
         }
         return new DNA(newgenes);
     }
+    this.mutation = function(){
+        for(var i =0;i<this.genes.length; i++){
+            this.genes[i] = p5.Vector.random2D();
+            this.genes[i].setMag(maxForce);
+        } 
+    }
+
 }
 
 
@@ -112,6 +135,8 @@ function Rocket(dna){
     this.pos = createVector(width/2, height);
     this.vel = createVector();
     this.acc = createVector();
+    this.completed = false;
+    this.crashed = false;
     if(dna)
         this.dna = dna;
     else
@@ -126,13 +151,40 @@ function Rocket(dna){
     this.calcFitness = function(){
         var d = dist(this.pos.x, this.pos.y, target.x, target.y)
         this.fitness = map(d,0,width,width,0);
+        if(this.completed){
+            this.fitness *= 10;
+        }
 
+        if(this.crashed){
+            this.fitness = 1;
+        }
     }
     this.update = function(){
+
+        var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+        if(d<10){
+            this.completed = true;
+            this.pos = target.copy();
+        }
+
+        if(this.pos.x > rx && this.pos.x < rx+rh && this.pos.y >ry && this.pos.y<ry+rh){
+            this.crashed  = true;
+        }
+
+        if(this.pos.x > width || this.pos.x <0){
+            this.crashed = true;
+        }
+        if(this.pos.y >height|| this.pos.y <0){
+            this.crashed = true;
+        }
+        
+
         this.applyForce(this.dna.genes[count]);
+        if(!this.completed && !this.crashed){
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.mult(0);
+        }
     }
     this.show = function(){
         push();
@@ -141,7 +193,7 @@ function Rocket(dna){
         translate(this.pos.x, this.pos.y);
         rotate(this.vel.heading());
         rectMode(CENTER);
-        rect(0,0,50,10);
+        rect(0,0,40,8);
         pop();
     }
 }
